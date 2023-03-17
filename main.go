65a49911229c2edddd8b1fb52e5ccd5844176e8a
JunkiMakiuchi <junki.makiuchi@gmail.com>
2023-03-18 02:03:01 +0900

    抽出した文字からmacでsleepの実行

--- main.go
+++ main.go
@@ -5,31 +5,37 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func main() {
-
 	file, err := os.Open("/Users/makiuchijunki/Dropbox/macOSIFTTTControl/macaction.txt")
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 	defer file.Close()
 
 	//Read the file line by line and get the last line
-	//var lastLine string
 	scanner := bufio.NewScanner(file)
+	if scannerErr := scanner.Err(); scannerErr != nil {
+		log.Fatal(err)
+		return
+	}
 	for scanner.Scan() {
 		//Extract the string after "|" from the last line
 		lastLine := strings.Split(scanner.Text(), "|")
-		if len(lastLine) > 1 {
-			fmt.Println(lastLine[1])
+
+		//Argument to mac action
+		if lastLine[1] == "sleep" {
+			macsleep := exec.Command("sudo", "pmset", "sleepnow")
+			macsleepErr := macsleep.Run()
+			if macsleepErr != nil {
+				log.Fatal(err)
+				return
+			}
+			fmt.Println("Successfully put macOS to sleep.")
 		}
-
 	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
 }
